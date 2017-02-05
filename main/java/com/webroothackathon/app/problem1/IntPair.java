6d3eaba387a78e7240e79f748942df9e0c0fb4ae
William Tran <wbtran@ucsd.edu>
2017-02-04 16:48:06 -0800

    problem1 fixed

--- main/java/com/webroothackathon/app/problem1/IntPair.java
+++ main/java/com/webroothackathon/app/problem1/IntPair.java
@@ -2,38 +2,38 @@ package com.webroothackathon.app.problem1;
 
 public class IntPair
 {
-	public IntPair(int first, int second)
-	{
-		this.first = first;
-		this.second = second;
-	}
+  public IntPair(int first, int second)
+  {
+    this.first = first;
+    this.second = second;
+  }
 
-	@Override
-	public boolean equals(Object obj)
-	{
-		if (obj == null)
-			return false;
+  @Override
+    public boolean equals(Object obj)
+    {
+      if (obj == null)
+        return false;
 
-		if (!IntPair.class.isInstance(obj))
-			return false;
+      if (!IntPair.class.isInstance(obj))
+        return false;
 
-		IntPair other = (IntPair) obj;
+      IntPair other = (IntPair) obj;
 
-		return this.first == other.first && this.second == other.second;
-	}
-	
-	@Override
-	public int hashCode()
-	{
-		return (this.first * 17) + (this.second * 53);
-	}
+      return this.first == other.first && this.second == other.second;
+    }
 
-	@Override
-	public String toString()
-	{
-		return "(" + this.first + "," + this.second + ")";
-	}
+  @Override
+    public int hashCode()
+    {
+      return (this.first * 17) + (this.second * 53);
+    }
 
-	public int first;
-	public int second;
+  @Override
+    public String toString()
+    {
+      return "(" + this.first + "," + this.second + ")";
+    }
+
+  public int first;
+  public int second;
 }
--- main/java/com/webroothackathon/app/problem1/Problem1.java
+++ main/java/com/webroothackathon/app/problem1/Problem1.java
@@ -2,45 +2,57 @@ package com.webroothackathon.app.problem1;
 
 public class Problem1
 {
-	/*
-	 *  Takes in an array of UrlCatItems and picks out the smallest UrlCatItem 
-     	 *  from the array following these rules:
-	 *  1. urlCatItem with the smaller char(0) of the url is considered smaller.
-	 *  2. If both urls have the same char(0) of the url, then the one with 
-     	 *      lower reputation is considered smaller.
-	 *  3. If reputation is also equal, then the item that has a smaller index 
-  	 *      in the array is considered smaller.
-	 *	
-	 *  If an empty array is passed in an IntPair object with -1 category and 
-     	 *      -1 reputation should be returned.
-	 *
-	 *  @param (urlCatItems) (array of UrlCatItem)
-	 *  @return (an IntPair, where the first int is the category of the 
-     	 *      smallest UrlCatItem from the array and the second int is the 
-     	 *      reputation of the smallest urlCatItem from the array.)
-	 */
-	public static IntPair getLowestCatRep(UrlCatItem[] urlCatItems)
-	{
-        if (urlCatItems.length == 0) {
-            return new IntPair(-1, -1);
-        }
-        else {
-
-	        UrlCatItem lowestCatItem = urlCatItems[urlCatItems.length - 1];
-	           
-	        for(int i = urlCatItems.length - 2; i > 0; i--)
-	        {
-	        	if (lowestCatItem.isGreater(urlCatItems[i]))
-	        		lowestCatItem = urlCatItems[i];
-	        }
-	        return new IntPair(lowestCatItem.category, lowestCatItem.reputation);
-        }
-
-	}
-    /*
-    public static void main(String[] argv) {
-        UrlCatItem[] u = new UrlCatItem[0];
-        getLowestCatRep(u);
+  /*
+   *  Takes in an array of UrlCatItems and picks out the smallest UrlCatItem 
+   *  from the array following these rules:
+   *  1. urlCatItem with the smaller char(0) of the url is considered smaller.
+   *  2. If both urls have the same char(0) of the url, then the one with 
+   *      lower reputation is considered smaller.
+   *  3. If reputation is also equal, then the item that has a smaller index 
+   *      in the array is considered smaller.
+   *	
+   *  If an empty array is passed in an IntPair object with -1 category and 
+   *      -1 reputation should be returned.
+   *
+   *  @param (urlCatItems) (array of UrlCatItem)
+   *  @return (an IntPair, where the first int is the category of the 
+   *      smallest UrlCatItem from the array and the second int is the 
+   *      reputation of the smallest urlCatItem from the array.)
+   */
+  public static IntPair getLowestCatRep(UrlCatItem[] urlCatItems)
+  {
+    if (urlCatItems.length == 0) {
+      return new IntPair(-1, -1);
     }
-    */
+    else {
+
+      UrlCatItem lowestCatItem = urlCatItems[urlCatItems.length - 1];
+
+      for(int i = urlCatItems.length - 2; i >= 0; i--)
+      {
+        if (lowestCatItem.isGreater(urlCatItems[i]))
+          lowestCatItem = urlCatItems[i];
+      }
+      return new IntPair(lowestCatItem.category, lowestCatItem.reputation);
+    }
+
+  }
+/*
+  public static void main(String[] argv) {
+    UrlCatItem[] u = new UrlCatItem[0];
+    IntPair test = getLowestCatRep(u);
+    System.out.println(test.first);
+    System.out.println(test.second);
+
+    UrlCatItem[] urlArr = new UrlCatItem[5];
+    urlArr[0] = new UrlCatItem("abc", 0, 100);
+    urlArr[1] = new UrlCatItem("abc", 7, 100);
+    urlArr[2] = new UrlCatItem("abc", 7, 100);
+    urlArr[3] = new UrlCatItem("abc", 7, 100);
+    urlArr[4] = new UrlCatItem("abc", 7, 100);
+    IntPair test1 = getLowestCatRep(urlArr);
+    System.out.println(test1.first);
+    System.out.println(test1.second);
+  }
+*/
 }
